5268c98b7f9797b0e291a85df4c47ac9554250b4
Руслан Каписъски <ruslank1@abv.bg>
2025-10-10 15:25:04 +0300

    Connect to database

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import express, { request } from "express";
 import handlebars from "express-handlebars";
 
 import routes from "./routes.js";
+import mongoose from "mongoose";
 
 const app = express();
 
+//setup database
+const url = "mongodb://localhost:27017";
+try {
+  await mongoose.connect(url, {
+    dbName: "movie-magic",
+  });
+
+  console.log("Successfuly connected to database");
+} catch (error) {
+  console.error("Problem with connecting to database:", error.message());
+}
 //setup handlebars
 app.engine(
-	"hbs",
-	handlebars.engine({
-		extname: "hbs",
-	})
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
 );
 
 //set view engine
@@ -23,9 +35,9 @@ app.use(express.urlencoded());
 
 app.use(routes);
 app.get("*splat", (req, res) => {
-	res.render("404");
+  res.render("404");
 });
 
 app.listen(5000, () =>
-	console.log("Server is listening on http://localhost:5000...")
+  console.log("Server is listening on http://localhost:5000...")
 );
