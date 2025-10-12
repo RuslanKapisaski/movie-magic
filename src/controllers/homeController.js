bb5ca54a321266087c9906e188b5883202de0387
Руслан Каписъски <ruslank1@abv.bg>
2025-10-12 17:28:10 +0300

    Add greeting user message

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,7 +6,11 @@ const homeController = Router();
 
 homeController.get("/", async (req, res) => {
 	const movies = await movieService.getAll();
-	res.render("home", { movies, isAuthenticated: req.isAuthenticated });
+	res.render("home", {
+		movies,
+		isAuthenticated: req.isAuthenticated,
+		user: req.user,
+	});
 });
 
 homeController.get("/about", (req, res) => {
